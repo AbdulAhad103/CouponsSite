b99679fe6697a97c6c937f490c086ce8c54e9978
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-01-15 01:58:40 +0500

    Add categories

--- riyadh-coupons/src/app/components/CategoryPage.jsx
+++ riyadh-coupons/src/app/components/CategoryPage.jsx
@@ -4,12 +4,39 @@ import Airtable from "airtable";
 import DiscountCard from "./DiscountCard";
 
 const CategoryPage = ({ activeCategory }) => {
-
   const idToName = (id) => {
     if (id === "recwNHt03bNMaxEqg") return "زارا";
     if (id === "recU3rCFXf6FhhWau") return "الخطوط الجوية التركية";
     if (id === "recFd4c7bZYeDjPsD") return "السيف غاليري";
     if (id === "rec5XNRPavPNJkT9C") return "تومي هيلفيغر";
+  };
+
+  const [currId, setCurrId] = useState();
+
+  const idToCategory = (base, category) => {
+    base("CATEGORY")
+      .select({
+        view: "Grid view",
+      })
+      .eachPage(function page(records, fetchNextPage) {
+        records.forEach(function (record) {
+          try {
+            const field = record.get("Name");
+            if (field && field === category) {
+              console.log("FI", record.id);
+              setCurrId(record.id);
+            }
+          } catch (e) {
+            console.log("ERROR");
+          }
+        });
+        function done(err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+        }
+      });
   };
 
   const [coupons, setCoupons] = useState([]);
@@ -27,10 +54,12 @@ const CategoryPage = ({ activeCategory }) => {
               headline: idToName(fields.SELLER[0]),
               discountPercentage: fields.discountPercentage,
               isCoupon: true,
+              category: fields.Category[0],
               couponCode: fields.DiscountCode,
               referralLink: "",
             };
-            console.log(record);
+            // console.log(record);
+            console.log(obj);
             setCoupons([...coupons, obj]);
           } catch (e) {
             console.log("ERROR");
@@ -60,6 +89,7 @@ const CategoryPage = ({ activeCategory }) => {
               image: fields.IMAGE[0].url,
               headline: idToName(fields.SELLER[0]),
               discountPercentage: fields.discountPercentage,
+              category: fields.Category[0],
               isCoupon: false,
               couponCode: "",
               referralLink: fields.ReferralLinks,
@@ -78,30 +108,46 @@ const CategoryPage = ({ activeCategory }) => {
       });
   }
 
-  useEffect(()=> {
+  useEffect(() => {
     var base = new Airtable({
       apiKey:
         "pat6ctkSNfMS5DX6L.46279e5c455dfb85f9d04d3dc9914a0f5aa7c2038ab04a05a92b0f6883104094",
     }).base("appcKk3YBb5YRubtz");
+    idToCategory(base, activeCategory);
+    console.log(activeCategory);
     fetchCoupons(base);
     fetchReferrals(base);
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    console.log(activeCategory);
+    var base = new Airtable({
+      apiKey:
+        "pat6ctkSNfMS5DX6L.46279e5c455dfb85f9d04d3dc9914a0f5aa7c2038ab04a05a92b0f6883104094",
+    }).base("appcKk3YBb5YRubtz");
+    idToCategory(base, activeCategory);
+    console.log("CURR:", currId);
+  }, [activeCategory]);
 
   return (
     <div>
       <h2 className="text-right text-4xl mb-2">كوبونات الخصم</h2>
       <h3 className="text-right text-[#9A9A9A] text-3xl mb-10">جرب التوفير</h3>
       <div className="flex justify-end gap-3 items-center flex-wrap max-w-[1658px]">
-        {coupons.map((coupon, index) => (
-          <DiscountCard {...coupon} key={index} />
-        ))}
+        {coupons.map((coupon, index) => {
+          if (coupon.category === currId) {
+            return <DiscountCard {...coupon} key={index} />;
+          } else <></>;
+        })}
       </div>
       <h2 className="text-right text-4xl mb-2 mt-24">عروض المتاجر</h2>
       <h3 className="text-right text-[#9A9A9A] text-3xl mb-10">متاجر مميزة</h3>
       <div className="flex justify-end gap-3 items-center flex-wrap max-w-[1658px]">
-        {referrals.map((referral, index) => (
-          <DiscountCard {...referral} key={index} />
-        ))}
+        {referrals.map((referral, index) => {
+          if (referral.category === currId) {
+            return <DiscountCard {...referral} key={index} />;
+          }
+        })}
       </div>
     </div>
   );
--- riyadh-coupons/src/app/components/Pagination.jsx
+++ riyadh-coupons/src/app/components/Pagination.jsx
@@ -1,12 +1,13 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Airtable from "airtable";
 import PaginationBtn from "./PaginationBtn";
 import CategoryPage from "./CategoryPage";
 import Search from "./Search";
 
 const Pagination = ({ coupons, referrals }) => {
   // Page Number
-  const [activeIndex, setActiveIndex] = useState(6);
+  const [activeIndex, setActiveIndex] = useState('المطاعم');
 
   const changeIndex = (index) => {
     setActiveIndex(index);
@@ -29,27 +30,50 @@ const Pagination = ({ coupons, referrals }) => {
 };
 
 const RenderPaginationBtns = ({ activeIndex, changeIndex }) => {
-  const [categories, setCategories] = useState([
-    "أخرى",
-    "مواد بناء",
-    "سوبر ماركت",
-    "السفر",
-    "الأواني",
-    "العطور",
-    "الجمال",
-    "المطاعم",
-    "التسوق",
-  ]);
+  const [categories, setCategories] = useState([])
+  const [categoryIDs, setCategoryIDs] = useState("")
+  function fetchCategories(base) {
+    base("CATEGORY")
+      .select({
+        view: "Grid view",
+      })
+      .eachPage(function page(records, fetchNextPage) {
+        records.forEach(function (record) {
+          try {
+            const field = record.get("Name")
+            if(field){
+              setCategories(categories => [...categories, field])
+              setCategoryIDs(categoryIDs => [...categoryIDs, record.id])
+            }
+          } catch (e) {
+            console.log("Record doesn't exist.");
+          }
+        });
+        function done(err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+        }
+      });
+  }
+  useEffect(()=> {
+    var base = new Airtable({
+      apiKey:
+        "pat6ctkSNfMS5DX6L.46279e5c455dfb85f9d04d3dc9914a0f5aa7c2038ab04a05a92b0f6883104094",
+    }).base("appcKk3YBb5YRubtz");
+    fetchCategories(base);
+  }, [])
   return (
     <div className="pagination w-full overflow-x-scroll flex justify-between items-center">
-      {categories.map((category, index) => {
+      {[...new Set(categories)].map((category, index) => {
         let isActive = false;
-        if (index === activeIndex) isActive = true;
+        if (category === activeIndex) isActive = true;
         return (
           <PaginationBtn
             key={index}
             active={isActive}
-            changeIndex={() => changeIndex(index)}
+            changeIndex={() => changeIndex(category)}
           >
             {category}
           </PaginationBtn>
