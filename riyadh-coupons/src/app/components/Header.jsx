e97e4206121dc4a569d4edf85de34a46f251bf35
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-01-14 18:02:20 +0500

    Fix logo

--- riyadh-coupons/src/app/components/Header.jsx
+++ riyadh-coupons/src/app/components/Header.jsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import Star from "./Star";
+import Logo from "../../../public/images/favicon.png"
 
 const Header = () => {
   const YELLOW_COLOR = "#FBC500"
@@ -7,9 +9,7 @@ const Header = () => {
     <header>
       <nav className="flex items-center justify-end gap-6 py-7">
         <h1 className="text-blue-800 text-6xl font-['29LTZaridSlab-Bold']">كوبون الرياض</h1>
-        <div className={`w-20 h-20 bg-[#021EDD] rounded-[50%] flex justify-center items-center`}>
-          <Star width={"53px"} color={YELLOW_COLOR} />
-        </div>
+        <Image className="w-16 h-16" src={Logo} />
       </nav>
       <div className={`flex justify-center items-center bg-[#FBC500] w-full max-h-[32rem] h-[50svh] rounded-3xl`}>
         <Star width={'190px'} color={BLUE_COLOR} />
